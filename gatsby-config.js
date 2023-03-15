90084688034329113fe56d671bf219d023215c7d
Benji <benjifriedman@gmail.com>
2023-03-14 23:39:27 -0700

    some changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 module.exports = {
   siteMetadata: {
-    title: "Gatsby Blog Demo",
+    title: "Benji Friedman Blog",
   },
   plugins: [
     "gatsby-plugin-postcss",
